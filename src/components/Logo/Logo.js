fc126ed98544bdba574e1d74ebab3f2cff035b24
Ali Akbar <63883870+Cheval5@users.noreply.github.com>
2021-05-15 15:31:44 -0400

    mlogo added

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import DetectLogo from '../../assets/logo/detective.svg'
+import './Logo.css';
 
 const Logo = () => {
     return (
         <div className="ma4 mt0 ">
-            <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner"> 👽 </div>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pa3">
+                    <img className="w5" src={DetectLogo} alt="logo" />
+                </div>
             </Tilt>
         </div>
     );
